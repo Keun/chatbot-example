c06feec22b4668d236c6b20622be2363cfa3e517
Koen <kborghols@glu.nl>
2019-12-19 14:52:15 +0100

    portnumbers openshift

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -13,6 +14,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, server_ip_address, function(){
+  console.log('listening on '+server_ip_address+':' + port);
 });
