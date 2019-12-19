f1e11b270688a203c1227562128411e98ddcdbff
Koen <kborghols@glu.nl>
2019-12-19 15:16:05 +0100

    error remove

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function(){
-  console.log('listening on '+server_ip_address+':' + port);
+  console.log('listening on *:' + port);
 });
