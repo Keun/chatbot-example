029995fc024ae839413265362725e23633aeeba3
Koen <kborghols@glu.nl>
2019-12-19 15:18:07 +0100

    port 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = 8080;
+var port = 80;
 
 
 app.get('/', function(req, res){
