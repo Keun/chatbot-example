1c4f4f88c095f859fa71b643ed6a2986ef505a21
Koen <kborghols@glu.nl>
2019-12-19 15:11:24 +0100

    new changes ip

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = 8080;
+var server_ip_address = '127.0.0.1';
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
